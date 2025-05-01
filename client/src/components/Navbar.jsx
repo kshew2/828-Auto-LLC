5e1d0ab1c7f7390f5424c7b750652c44175c022e
unknown <khalilshew1030@gmail.com>
2025-05-01 16:36:31 -0400

    ui changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -215,8 +215,26 @@ export const Navbar = () => {
                 )}
               </div> */}
               <ul className="">
-                <li className="py-1">Cell Phone: 828-238-4020</li>
-                <li className="py-1">Address: Gamewell, NC</li>
+              <li>
+                    <div className="flex items-center gap-3">
+                      <a
+                        href="tel:+18282384020"
+                        className="text-secondary-accent text-lg hover:text-secondary"
+                      >
+                        Phone: (828) 238-4020
+                      </a>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="flex items-center gap-2">
+                      <a
+                        href="https://www.google.com/maps/place/2788+Morganton+Blvd+SW,+Lenoir,+NC+28645/@35.8752307,-81.5918467,17z/data=!3m1!4b1!4m6!3m5!1s0x8850d94e1760b347:0xb9f8f5a45a1cc6df!8m2!3d35.8752307!4d-81.5918467!16s%2Fg%2F11j7lwyc8f?entry=ttu&g_ep=EgoyMDI1MDQyOS4wIKXMDSoASAFQAw%3D%3D"
+                        className="text-secondary-accent text-lg hover:text-secondary"
+                      >
+                        Address: Gamewell, NC
+                      </a>
+                    </div>
+                  </li>
               </ul>
             </div>
           </div>
